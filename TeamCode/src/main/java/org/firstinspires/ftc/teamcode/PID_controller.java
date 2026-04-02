78fa401b0849d9a6b32bace9a57239f9695dcf75
cavan <cavanlu5o3@gmail.com>
2026-04-02 16:23:31 -0700

    first commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID_controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID_controller.java
@@ -1,16 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 public class PID_controller {
     private double kP, kI, kD;
 
     private double integralSum = 0;
     private double lastError = 0;
 
-    public void init() {
+    private ElapsedTime timer = new ElapsedTime();
 
+    public PID_controller (double kP, double kI, double kD) {
+        this.kP = kP;
+        this.kI = kI;
+        this.kD = kD;
     }
 
-    public void loop() {
+    public double calculate(double target, double current) {
 
+        double error = target - current;
+        double derivative = (error -lastError) / timer.seconds();
+        integralSum += (error * timer.seconds());
+
+        if (Math.abs(error) < 1) { integralSum = 0; }
+
+        double output = (kP * error) + (kI * integralSum) + (kD * derivative);
+
+        lastError = error;
+        timer.reset();
+
+        return  output;
     }
 }
