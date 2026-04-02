b8a3f4836a3db36614eec8e5c5b9a1988283019f
cavan <cavanlu5o3@gmail.com>
2026-04-02 15:22:33 -0700

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCPractice.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public  class FTCPractice extends OpMode {
+
+    @Override
+    public void init(){
+        String button = "N/A";
+        boolean pressed = false;
+        while (!pressed) {
+            if (gamepad1.a) button = "a";
+            else if (gamepad1.b) button = "b";
+            else if (gamepad1.x) button = "x";
+            else if (gamepad1.y) button = "y";
+            pressed = true;
+        }
+        telemetry.addData("Pressed", button);
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
