c86571ff1e427b358633e00266f9a98451bbe87b
cavan <cavanlu5o3@gmail.com>
2026-04-02 17:52:46 -0700

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/real_autonomous.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.storage;
+
+@Autonomous(name = "WAutonomous")
+public class real_autonomous extends LinearOpMode {
+    private storage storage;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        storage = new storage();
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        while (opModeIsActive() && !isStopRequested()){
+
+            storage.runWithEncoderLeftB();
+            storage.runWithEncoderLeftF();
+            storage.runWithEncoderRightB();
+            storage.runWithEncoderRightF();
+
+            wait(5000);
+
+            storage.stopMotors();
+        }
+    }
+}
